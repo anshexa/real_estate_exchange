7312dc3fced1a80462232d43e83e99cc8e1321a4
anshexa <lilimahiya@gmail.com>
2023-09-11 12:00:02 +0300

    task 150328 - Внедрение верстки в компонент Слайдер в index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $filterIsPreferredDeal = array(
 );
 $APPLICATION->IncludeComponent(
     "bitrix:news.list",
-    ".default",
+    "slider",
     array(
         "ACTIVE_DATE_FORMAT" => "d.m.Y",
         "ADD_SECTIONS_CHAIN" => "N",
@@ -39,11 +39,11 @@ $APPLICATION->IncludeComponent(
         "DISPLAY_BOTTOM_PAGER" => "Y",
         "DISPLAY_DATE" => "N",
         "DISPLAY_NAME" => "Y",
-        "DISPLAY_PICTURE" => "Y",
-        "DISPLAY_PREVIEW_TEXT" => "Y",
+        "DISPLAY_PICTURE" => "N",
+        "DISPLAY_PREVIEW_TEXT" => "N",
         "DISPLAY_TOP_PAGER" => "N",
         "FIELD_CODE" => array(
-            0 => "",
+            0 => "DETAIL_PICTURE",
             1 => "",
         ),
         "FILTER_NAME" => "filterIsPreferredDeal",
@@ -53,19 +53,19 @@ $APPLICATION->IncludeComponent(
         "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
         "INCLUDE_SUBSECTIONS" => "N",
         "MESSAGE_404" => "",
-        "NEWS_COUNT" => "1",
+        "NEWS_COUNT" => "",
         "PAGER_BASE_LINK_ENABLE" => "N",
         "PAGER_DESC_NUMBERING" => "N",
         "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
         "PAGER_SHOW_ALL" => "N",
         "PAGER_SHOW_ALWAYS" => "N",
         "PAGER_TEMPLATE" => ".default",
-        "PAGER_TITLE" => "Новости",
+        "PAGER_TITLE" => "Объявления",
         "PARENT_SECTION" => "",
         "PARENT_SECTION_CODE" => "",
         "PREVIEW_TRUNCATE_LEN" => "",
         "PROPERTY_CODE" => array(
-            0 => "",
+            0 => "PRICE",
             1 => "",
         ),
         "SET_BROWSER_TITLE" => "N",
@@ -80,58 +80,12 @@ $APPLICATION->IncludeComponent(
         "SORT_ORDER1" => "DESC",
         "SORT_ORDER2" => "ASC",
         "STRICT_SECTION_CHECK" => "N",
-        "COMPONENT_TEMPLATE" => ".default",
+        "COMPONENT_TEMPLATE" => "slider",
         "FILE_404" => "/404.php"
     ),
     false
 );
 ?>
-    <div class="slide-one-item home-slider owl-carousel">
-
-        <div class="site-blocks-cover" style="background-image: url(<?= SITE_DIR ?>upload/hero_bg_1.jpg);"
-             data-aos="fade" data-stellar-background-ratio="0.5">
-
-            <div class="text">
-                <h2>853 S Lucerne Blvd</h2>
-                <p class="location">
-                    <span class="property-icon icon-room"></span>
-                    Los Angeles, CA 90005
-                </p>
-                <p class="mb-2"><strong>$2,250,500</strong></p>
-
-
-                <p class="mb-0">
-                    <a href="#" class="text-uppercase small letter-spacing-1 font-weight-bold">
-                        More Details
-                    </a>
-                </p>
-
-            </div>
-        </div>
-
-        <div class="site-blocks-cover" style="background-image: url(<?= SITE_DIR ?>upload/hero_bg_3.jpg);"
-             data-aos="fade" data-stellar-background-ratio="0.5">
-
-            <div class="text">
-                <h2>625 S. Berendo St</h2>
-                <p class="location">
-                    <span class="property-icon icon-room"></span>
-                    607 Los Angeles, CA 90005
-                </p>
-                <p class="mb-2"><strong>$2,250,500</strong></p>
-
-
-                <p class="mb-0">
-                    <a href="#" class="text-uppercase small letter-spacing-1 font-weight-bold">
-                        More Details
-                    </a>
-                </p>
-
-            </div>
-
-        </div>
-
-    </div>
 
     <div class="py-5">
         <div class="container">
---
+++ local/templates/home/components/bitrix/news.list/slider/lang/en/template.php
@@ -0,0 +1,4 @@
+<?
+$MESS["MORE_DETAILS"] = "More Details";
+$MESS["CT_BNL_ELEMENT_DELETE_CONFIRM"] = "All the information linked to this record will be deleted. Continue anyway?";
+?>
