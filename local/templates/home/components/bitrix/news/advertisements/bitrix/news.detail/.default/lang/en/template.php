912c352c7a8890346c509f384be6cd68c835ff26
anshexa <lilimahiya@gmail.com>
2023-09-17 14:27:44 +0300

    task 150330 - Внедрение верстки для детальной страницы объявлений в разделе объявлений

---
+++ local/templates/home/components/bitrix/news/advertisements/bitrix/news.detail/.default/lang/en/template.php
@@ -0,0 +1,10 @@
+<?
+$MESS["UPDATE_DATE"] = "Update date";
+$MESS["NUMBER_OF_FLOORS"] = "Number of floors";
+$MESS["TOTAL_AREA"] = "Total area";
+$MESS["METER"] = "m";
+$MESS["NUMBER_OF_BATHROOMS"] = "Number of bathrooms";
+$MESS["AVAILABILITY_OF_GARAGE"] = "Availability of garage";
+$MESS["MORE_INFO"] = "More Info";
+$MESS["PROPERTY_GALLERY"] = "Property Gallery";
+?>
---
+++ local/templates/home/components/bitrix/news/advertisements/bitrix/news.detail/.default/lang/ru/template.php
@@ -0,0 +1,10 @@
+<?
+$MESS["UPDATE_DATE"] = "Дата обновления";
+$MESS["NUMBER_OF_FLOORS"] = "Этажей";
+$MESS["TOTAL_AREA"] = "Общая площадь";
+$MESS["METER"] = "м";
+$MESS["NUMBER_OF_BATHROOMS"] = "Количество санузлов";
+$MESS["AVAILABILITY_OF_GARAGE"] = "Наличие гаража";
+$MESS["MORE_INFO"] = "Описание";
+$MESS["PROPERTY_GALLERY"] = "Property Gallery";
+?>
