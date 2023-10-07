e97d623142a40fa09324de3d488d901b92ce28f5
anshexa <lilimahiya@gmail.com>
2023-10-07 17:06:24 +0300

    task 150337 - [ex1-19] Создание пользователя с ограниченным доступом: редактор отзывов

--- local/templates/exam1/components/bitrix/news/exam1_reviews/bitrix/news.list/.default/template.php
+++ local/templates/exam1/components/bitrix/news/exam1_reviews/bitrix/news.list/.default/template.php
@@ -17,8 +17,29 @@ $this->setFrameMode(true);
 
 <?foreach($arResult["ITEMS"] as $arItem):?>
     <?
-    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+    // добавляем кнопку "добавить элемент"
+    $arButtons = CIBlock::GetPanelButtons(
+        $arItem["IBLOCK_ID"],
+        $arItem["ID"],
+        0,
+        array("SECTION_BUTTONS"=>false, "SESSID"=>false)
+    );
+    $arItem["ADD_LINK"] = $arButtons["edit"]["add_element"]["ACTION_URL"];
+    $this->AddEditAction(
+        $arItem['ID'],
+        $arItem['ADD_LINK'],
+        CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_ADD"),
+        array("ICON" => "bx-context-toolbar-create-icon",));
+
+    $this->AddEditAction(
+        $arItem['ID'],
+        $arItem['EDIT_LINK'],
+        CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+    $this->AddDeleteAction(
+        $arItem['ID'],
+        $arItem['DELETE_LINK'],
+        CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"),
+        array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
     ?>
     <div class="review-block" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
         <div class="review-text">
