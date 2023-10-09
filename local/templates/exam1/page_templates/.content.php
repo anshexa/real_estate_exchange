86cd702d70f03cef01e8ff734649eaab95667ee4
anshexa <lilimahiya@gmail.com>
2023-10-09 20:01:50 +0300

    task 150338 - [ex1-10] Шаблоны для контента статичной страницы: демоконтент inner.html

---
+++ local/templates/exam1/page_templates/.content.php
@@ -0,0 +1,7 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+} ?>
+<?
+IncludeTemplateLangFile(__FILE__);
+$TEMPLATE["exam1_inner.php"] = array("name" => GetMessage("exam1_inner"), "sort" => 2);
+?>
