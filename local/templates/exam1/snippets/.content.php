ff19b98ec7b2550760fc930c2dad31b21cacb1ee
anshexa <lilimahiya@gmail.com>
2023-10-10 20:03:34 +0300

    task 150338 - [ex1-12] Ускорение работы с контентом: html заготовка с контента inner.html

---
+++ local/templates/exam1/snippets/.content.php
@@ -0,0 +1,7 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+} ?>
+<?
+$SNIPPETS = array();
+$SNIPPETS['snippet0001.snp'] = array('title' => 'html заготовка с контента inner.html');
+?>
